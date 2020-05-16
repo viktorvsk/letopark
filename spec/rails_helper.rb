c40672ed3f28a2e5d027f98c3d5168f94077d0b3
viktorvsk <me@viktorvsk.com>
2020-05-16 23:54:44 +0300

    Init Airbrake

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,7 +33,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
