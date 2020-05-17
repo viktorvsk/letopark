17a32cf2414b775fc3dcc84adea492d2370b220a
viktorvsk <me@viktorvsk.com>
2020-05-17 11:56:44 +0300

    Init rack-cors for all domains

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,12 @@ module LetoparkServer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
   end
 end
