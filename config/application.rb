107a9e44e280b4b8441c51b41a4635525a9827c0
viktorvsk <me@viktorvsk.com>
2020-05-19 13:58:57 +0300

    Fix rack-cors setup; [WIP]

--- config/application.rb
+++ config/application.rb
@@ -25,12 +25,5 @@ module LetoparkServer
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: %i[get post options]
-      end
-    end
   end
 end
