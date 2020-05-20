09b73cad8300444bb8841fc3d80760692c03e47b
viktorvsk <me@viktorvsk.com>
2020-05-21 00:48:18 +0300

    Current Customer orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -3,7 +3,7 @@
 module Api
   module V1
     class OrdersController < ApiController
-      before_action :set_current_device, only: %w[create]
+      before_action :set_current_device, only: %w[index create]
 
       def create
         order = current_device.customer.orders.create!(comment: params[:order][:comment])
@@ -12,6 +12,10 @@ module Api
           order.order_items.create!(item_params)
         end
 
+        render json: current_device.customer.orders
+      end
+
+      def index
         render json: current_device.customer.orders
       end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :stores, only: %w[index]
-      resources :orders, only: %w[create]
+      resources :orders, only: %w[create index]
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
